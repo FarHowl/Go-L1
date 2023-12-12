30763603ab0c2d665e28b83bbb6ccac6f4452125
Kartashov Dmitriy <kartashov104@gmail.com>
2023-12-12 10:13:18 +0300

    Fix 22 ex

--- 22/22.go
+++ 22/22.go
@@ -5,32 +5,6 @@ import (
 	"math/big"
 )
 
-// func calculate[T any](a, b T) {
-// 	var sum, sub, div, mul T
-// 	sum.Add(&a, &b)
-// 	sub.Sub(&a, &b)
-// 	div.Div(&a, &b)
-// 	mul.Mul(&a, &b)
-
-// 	fmt.Println("Sum: ")
-// 	fmt.Println(sum.String())
-// 	fmt.Println("Subdivision: ")
-// 	fmt.Println(sub.String())
-// 	fmt.Println("Division: ")
-// 	fmt.Println(div.String())
-// 	fmt.Println("Multiplication: ")
-// 	fmt.Println(mul.String())
-// }
-
-// type BigType[T any] interface {
-// 	Add()
-// 	Sub()
-// 	Div()
-// 	Mul()
-// }
-
-// func (b *BigType[T]) Add
-
 func main() {
 	var a big.Int
 	a.SetString("1234567890123456789012345678901234567890", 10)
